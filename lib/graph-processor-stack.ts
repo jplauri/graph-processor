b265508640b3a7bcad3218d940c6fdf29c219928
Juho Lauri <juho.lauri@gmail.com>
2022-05-21 22:14:47 +0300

    add s3 bucket and sqs queue

--- lib/graph-processor-stack.ts
+++ lib/graph-processor-stack.ts
@@ -1,16 +1,21 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Duration } from 'aws-cdk-lib';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import { BlockPublicAccess } from 'aws-cdk-lib/aws-s3';
+
 
 export class GraphProcessorStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const input_bucket = new s3.Bucket(this, 'InputBucket', {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'GraphProcessorQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const queue = new sqs.Queue(this, 'GraphProcessorQueue', {
+       visibilityTimeout: Duration.seconds(300)
+    });
   }
 }
