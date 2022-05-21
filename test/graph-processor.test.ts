0c936a6676280246d4f9915f09ecf238beb1e237
Juho Lauri <juho.lauri@gmail.com>
2022-05-21 15:00:48 +0300

    Initial commit

---
+++ test/graph-processor.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GraphProcessor from '../lib/graph-processor-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/graph-processor-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GraphProcessor.GraphProcessorStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
