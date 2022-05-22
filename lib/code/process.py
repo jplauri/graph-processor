d63992c58c9f4884ec8c2b8e7414dee43e229052
Juho Lauri <juho.lauri@gmail.com>
2022-05-22 16:45:02 +0300

    process input graph via nx

--- lib/code/process.py
+++ lib/code/process.py
@@ -4,12 +4,11 @@ def compute_properties(event, context):
     try:
         for record in event['Records']:
             payload = record["body"]
-            print(str(payload))
 
-            #g = nx.from_graph6_bytes(payload.encode())
-            #n = nx.number_of_nodes(g)
-            #m = nx.number_of_edges(g)
-            #print(f"for {payload} it holds that n = {n}, m = {m}")
+            g = nx.from_graph6_bytes(payload.encode())
+            n = nx.number_of_nodes(g)
+            m = nx.number_of_edges(g)
+            print(f"for {payload} it holds that n = {n}, m = {m}")
 
     except Exception as e:
         print(f"Exception: {e}")
