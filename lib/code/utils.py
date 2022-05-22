4a4ed11cbcab9b94ef7c59c21244fe7c2d1e4c3a
Juho Lauri <juho.lauri@gmail.com>
2022-05-22 14:28:40 +0300

    compute properties from sqs stored graphs

--- lib/code/utils.py
+++ lib/code/utils.py
@@ -1,13 +1,13 @@
 import os
 import urllib.parse
 import boto3
-
+import networkx as nx
 
 s3 = boto3.client('s3')
 sqs = boto3.client('sqs')
 
 
-def handler(event, context):
+def preprocess(event, context):
     # Get the object from the event and show its content type
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
@@ -27,6 +27,22 @@ def handler(event, context):
             print(response)
 
         return {"message": "Success"}
+    except Exception as e:
+        print(f"Exception: {e}")
+        raise e
+
+
+def compute_properties(event, context):
+    try:
+        payload = event['Records']["body"]
+        print(payload)
+
+        g = nx.from_graph6_bytes(payload.encode())
+        n = nx.number_of_nodes(g)
+        m = nx.number_of_edges(g)
+
+        print(f"for {payload} it holds that n = {n}, m = {m}")
+
     except Exception as e:
         print(f"Exception: {e}")
         raise e
--- lib/graph-processor-stack.ts
+++ lib/graph-processor-stack.ts
@@ -1,6 +1,7 @@
 import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Duration } from 'aws-cdk-lib';
+import { SqsEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3n from 'aws-cdk-lib/aws-s3-notifications';
@@ -23,7 +24,7 @@ export class GraphProcessorStack extends Stack {
 
     const preprocessor = new lambda.Function(this, 'InputPreprocessor', {
       runtime: lambda.Runtime.PYTHON_3_8,
-      handler: 'utils.handler',
+      handler: 'utils.preprocess',
       code: lambda.Code.fromAsset(path.join(__dirname, 'code')),
       retryAttempts: 0,
       environment: { "target_queue": queue.queueName }
@@ -37,5 +38,16 @@ export class GraphProcessorStack extends Stack {
     });
 
     queue.grantSendMessages(preprocessor);
+
+    const compute_properties = new lambda.Function(this, 'ComputeProperties', {
+      runtime: lambda.Runtime.PYTHON_3_8,
+      handler: 'utils.compute_properties',
+      code: lambda.Code.fromAsset(path.join(__dirname, 'code')),
+      retryAttempts: 0,
+      environment: { "target_queue": queue.queueName }
+    })
+
+    const eventSource = new SqsEventSource(queue);
+    compute_properties.addEventSource(eventSource);
   }
 }
